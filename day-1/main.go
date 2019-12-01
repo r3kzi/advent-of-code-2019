9659ae5c7caabf0e3b2371fb47b938712d8f2acb
Christian Niehoff <christian.niehoff@dwpbank.de>
2019-12-01 10:31:09 +0100

    added day-1

---
+++ day-1/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("day-1/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	
+	reader := bufio.NewReader(file)
+
+	var sum = 0
+	for {
+		mass, _, err := reader.ReadLine()
+		if err != nil {
+			break
+		}
+		byteToInt, _ := strconv.Atoi(string(mass))
+
+		fuel := CalculateFuel(byteToInt, 0)
+
+		sum += fuel
+	}
+	fmt.Println(sum)
+}
+
+func CalculateFuel(mass int, total int) int {
+	fuel := int((math.Floor(float64(mass)) / 3) - 2)
+	if fuel <= 0 {
+		// negative fuel = zero fuel
+		return total
+	}
+	total += fuel
+	return CalculateFuel(fuel, total)
+}
---
+++ day-1/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestFor1969(t *testing.T) {
+	mass := 1969
+	total := CalculateFuel(mass, 0)
+	if total != 966 {
+		t.Error("failed - should be 966")
+	}
+}
+
+func TestFor14(t *testing.T) {
+	mass := 14
+	total := CalculateFuel(mass, 0)
+	if total != 2 {
+		t.Error("failed - should be 2")
+	}
+}
+
+func TestFor100756(t *testing.T) {
+	mass := 100756
+	total := CalculateFuel(mass, 0)
+	if total != 50346 {
+		t.Error("failed - should be 50346")
+	}
+}
